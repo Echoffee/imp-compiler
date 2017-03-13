1a1f501002b50dcd4871e6ae4dcb036d60e93b28
Echoffee <light.runa@gmail.com>
2017-03-13 23:16:30 +0100

    Update on cc3a, works on pex3 Addresses are different tho

--- c3a_compiler.c
+++ c3a_compiler.c
@@ -183,7 +183,18 @@ void c3a_execute()
 			break;
 			
 			case PL:
-			
+			{
+				char v1[16];
+				sprintf(v1, "%d(%%edx)", get_var_pos(c_root->arg1));
+				char v2[16];
+				sprintf(v2, "%d(%%edx)", get_var_pos(c_root->arg2));
+				char v3[16];
+				sprintf(v3, "%d(%%edx)", get_var_pos(c_root->dst));
+				output_write(c_root->etq, "mrmovl", v1, "%%eax");
+				output_write("", "mrmovl", v2, "%%ebx");
+				output_write("", "addl", "%%eax", "%%ebx");
+				output_write("", "rmmovl", "%%ebx", v3);
+			}
 			break;
 			
 			case MO:
